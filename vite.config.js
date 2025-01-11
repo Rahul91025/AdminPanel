7d212d377265b1834aed66799377dc9fa6cfd804
Rahul Gupta <rahulguptacsit@gmail.com>
2025-01-11 15:08:37 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5174, // Default port for local development
-    host: true, // Allow access from external hosts
-    strictPort: false, // Allow dynamic port binding
-    watch: {
-      usePolling: true, // Ensure proper behavior in Docker/CI environments like Render
-    },
-  },
-  preview: {
-    port: parseInt(process.env.PORT) || 5174, // Use Render's assigned port or default to 5174
-    host: true,
-  },
-});
+  server: {port: 5174}
+})
